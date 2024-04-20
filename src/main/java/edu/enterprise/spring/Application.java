defb7ea565906df5190a8fbdca58b05f0d4f800c
Saulolo <saulolo@gmail.com>
2024-04-19 23:29:18 -0500

    🎉 Inicio el proyecto con el primer: Hola Mundo

--- src/main/java/edu/enterprise/spring/Application.java
+++ src/main/java/edu/enterprise/spring/Application.java
@@ -2,12 +2,24 @@ package edu.enterprise.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+//[4].Excluye el @SpringBootApplication que es la seguridad de autenticación por el
+//momento para poder imprimir el hola mundo
+@SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
 public class Application {
+
+	@GetMapping("/hello")
+	public String saludar() {
+		return "Hola Mundo con spring boot";
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
 
+	}
 }
